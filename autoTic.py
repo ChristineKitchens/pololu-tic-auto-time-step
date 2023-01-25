75ccdfc3bd3f8690b6084f39c4721798dc20e565
Christine K <christine.kitchens@noaa.gov>
2023-01-25 15:54:15 -0500

    Adding start and stop functions, user input to prompt shutdown.

--- autoTic.py
+++ autoTic.py
@@ -1,51 +1,86 @@
+import csv
 import pytic
 import time
 
+# - Functions ------------------------------------------------
+
+# Energize motor
+def start():
+    tic.energize()
+    tic.exit_safe_start()
+    print('Energizing motor...')
+
+# De-energize motor and get error status
+def shutdown():
+    tic.enter_safe_start()
+    tic.deenergize()
+    print('Deenergizing motor...')
+    print(tic.variables.error_status)
+
 # - Initialization -------------------------------------------
+
 
 tic = pytic.PyTic()
 
 # Connect to first available Tic Device serial number over USB
 serialNums = tic.list_connected_device_serial_numbers()
-print(serialNums[0])
 tic.connect_to_serial_number(serial_number=serialNums[0])
 
 # Load configuration file and apply settings
 tic.settings.load_config("config.yml")
 tic.settings.apply()
 
+# - Load in Settings -----------------------------------------
+
+variable_csv = r'run_parameters.csv'
+
+with open(variable_csv, encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    total_time_col = {'total_time': []}
+    velocity_col = {'target_velocities': []}
+    holding_col = {'holding_time': []}
+
+    for record in reader:
+        total_time_col['total_time'].append(record['total_time'])
+        velocity_col['target_velocities'].append(record['target_velocities'])
+        holding_col['holding_time'].append(record['holding_time'])
+
+
 # - Set Start Time -------------------------------------------
-while True:
-    try:
-        totalTimePeriod = float(
-            input("What total time interval (in minutes) is the impellar running for?"))
-        break
-    except ValueError:
-        print('Please enter a number.')
+# while True:
+#     try:
+#         totalTimePeriod = float(
+#             input("Enter holding time for each target velocity"))
+#         print(f'Holding Time: {totalTimePeriod} minutes.')
+#         break
+#     except ValueError:
+#         print('Please enter a number.')
 
-while True:
-    try:
-        maxSpeed = float(
-            input("What is the desired max speed of the impellar"))
-        break
-    except ValueError:
-        print('Please enter a number.')
+# while True:
+#     try:
+#         maxSpeed = float(
+#             input("What is the desired max speed of the impellar"))
+#         print(f'Max speed: {maxSpeed}')
+#         break
+#     except ValueError:
+#         print('Please enter a number.')
 
-while True:
-    try:
-        startSpeed = float(
-            input("What is the desired start speed of the impellar?"))
-        break
-    except ValueError:
-        print('Please enter a number.')
-
-print(totalTimePeriod)
-startTime = time.time()
+# while True:
+#     try:
+#         startSpeed = float(
+#             input("What is the desired start speed of the impellar?"))
+#         print(f'Start Speed: {startSpeed}')
+#         break
+#     except ValueError:
+#         print('Please enter a number.')
 
 
-while True:
-    print(time.time())
-    time.sleep(60.0 - ((time.time() - startTime) % 60.0))
+# startTime = time.time()
+# endTime =
+
+# output = open(f'sediment_experiment_{time.time()}', 'a')
+
 
 # - Motion Command Sequence ----------------------------------
 
@@ -53,17 +88,28 @@ while True:
 tic.halt_and_set_position(0)
 
 # Energize Motor
-tic.energize()
-tic.exit_safe_start()
+start()
 
-# Move to listed positions
-positions = [1000, 2000, 3000, 0]
-for p in positions:
-    tic.set_target_position(p)
-    while tic.variables.current_position != tic.variables.target_position:
-        time.sleep(0.1)
+tic.set_target_velocity(1000000)
+# tic.set_target_position(100)
+
+print(f'Target Velocity: {tic.variables.target_velocity}')
+print(f'Target Position: {tic.variables.target_position}')
+print(f'Current Velocity: {tic.variables.current_velocity}')
+print(f'Current Position: {tic.variables.current_position}')
+print(f'Planning Mode: {tic.variables.planning_mode}')
 
 # De-energize motor and get error status
-tic.enter_safe_start()
-tic.deenergize()
-print(tic.variables.error_status)
+while True:
+    try:
+        endPrompt = input('Stop Motor? Y/N  ')
+    except ValueError:
+        print('Incorrect input')
+    finally:
+        if endPrompt == 'Y' or endPrompt == 'y':
+            shutdown()
+            break
+        elif endPrompt == 'N' or endPrompt == 'n':
+            print("Use your spiral power!")
+        else:
+            print('Incorrect input')
