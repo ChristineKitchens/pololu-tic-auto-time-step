43bca65007b752c7adc9c517e0504a55a34a1b62
Christine K <christine.kitchens@noaa.gov>
2023-01-30 15:47:30 -0500

    Updated README. Merged in functionalities from PyTic development branch.

--- README.md
+++ README.md
@@ -1,23 +1,99 @@
-# Polulu Tic Automated Incremental Time Step
-Modification of Pololu Tic Control Center that automatically incrementally increases speed based on inputs. This script builds upon scripts in the [Pololu-Tic-Software Github](https://github.com/pololu/pololu-tic-software) and [PyTic Github](https://github.com/AllenInstitute/pytic).
+# Polulu Tic Automated Incremental Change in Target Velocity
+Script that automates incremental increases in velocity based on user inputs. This program builds upon scripts in the [Pololu-Tic-Software Github](https://github.com/pololu/pololu-tic-software) and [ticlib Github](https://github.com/jphalip/ticlib).
 
 # Prerequisites
-Requires [Tic Software and Drivers for Windows](https://www.pololu.com/file/0J1325/pololu-tic-1.8.2-win.msi) provided by Pololu to be installed. Also requires installation of [PyTic Python wrapper](https://github.com/AllenInstitute/pytic). Install the PyTic package and PyYAML dependency using the following script:
+Requires [Tic Software and Drivers for Windows](https://www.pololu.com/file/0J1325/pololu-tic-1.8.2-win.msi) provided by Pololu to be installed. Also requires installation of [ticlib Python wrapper](https://github.com/jphalip/ticlib). Install the libtic package and other dependencies using the following script:
 
 ```
 C:\> pip install pytic
-C:\> pip install pyyaml==5.4.1
+C:\> pip install csv
+C:\> pip install datetime
+C:\> pip install libusb_package
+C:\> pip install os
+C:\> pip install signal
+C:\> pip install sys
+C:\> pip install readchar
+C:\> pip install ticlib
+C:\> pip install time
 ```
-Make sure to install the 5.4.1 version of PyYAML specifically since newer versions require an additional argument that pytic does not account for.
-# Config.yml
-Use of the pytic package requires the creation of a config.yml file. To find appropriate codes for the PyYAML file, please reference the codes listed in the [pytic_protocol.py](https://github.com/AllenInstitute/pytic/blob/master/pytic/pytic_protocol.py) file
+# Script Use
+## 1. Update Settings via Pololu Tic Control Center
+Before running [autoTic.py](autoTic.py), open the Pololu Tic Control Center on the desktop. Navigate through the tabs and update the settings according to the product being used. For more details on settings and recommendations for given products, see the [Pololu Tic Manual](https://www.pololu.com/docs/0J71), Section 6 Setting Reference.
+
+<p align="center">
+  <img src="resources/tic_control_center1.png" width="500" />
+  <img src="resources/tic_control_center2.png" width="500" /> 
+  <img src="resources/tic_control_center3.png" width="500" />
+</p>
+
+After making any changes, make sure to click "Apply Settings" in the lower right-hand corner of the screen.
+
+## 2. Preparing Run Parameters
+The [autoTic.py](autoTic.py) allows the user to either import run parameters or manually enter them through the command line. After requesting a core ID, the script will prompt the user to select whether to import parameters or enter them into the command line:
+
+```
+> Import run parameters (I) or enter manually (M)? Enter I or M.
+```
+See below sections on guidance for both options.
+### <u>Import Run Parameters (Option A)</u>
+Update the [run_parameters.csv](run_parameters.csv) with desired target velocities and holding times. Keep in mind the maximum speed of the controller when selecting target velocities.
+* <b>target_velocities</b>: desired speed of the impeller in microsteps per 10,0000 seconds.
+* <b>holding_time</b>: the amount of time in seconds the user wants the impellar to maintain the target velocity.
+
+The script will proceed through the velocities and holding steps by row order.
+
+### <u>Manually Enter Run Parameters (Option B)</u>
+If the user wants to run the impellar on the fly without updating import documents, enter M at the prompt.
+
+```
+> Enter target velocities (microsteps/ 10,000 seconds):
+> Please enter a list separated by a space
+```
+Below is an example input:
+```
+50000 100000 150000 20000
+```
+The script will then ask for holding times for each target velocity.
+```
+Holding times for target velocities (secs):
+Please enter a list separated by a space
+```
+Below is an example input:
+```
+10 10 10 10
+```
+<b>Make sure that the length of the target velocities list matches the length of the holding times list.</b> Otherwise, this will cause a script error downstream.
+
+## 3. Executing Run
+After confirming run parameters are correct, the script will ask if the user is ready to begin the run.
+```
+Begin run? Y/N
+```
+After inputing `Y`, the script will start modifying the target velocities in accordance with the chosen run parameters. Note that pressing `Ctrl + C` at any time will shutdown the motor and exit the program.
+
+As the program runs, the current velocity, current time, and time until change in velocity will print to the console.
+
+![terminal_output](resources/terminal_output1.png)
+
+Simutaneously, the time and associated velocities will be written to a newly generated .txt file "sediment experiment core [user input] [current date]", with the variables depending on the current date and user input.
+
+## 4. Shutdown
+Once the holding time ends for the last specified velocity, the impellar will continue to run at the last velocity and a new prompt will ask the user if they want to shutdown the motor.
+```
+> Stop Motor? Y/N
+```
+Entering Y will shutdown the motor and exit the program. Again, note that pressing `Ctrl + C` at any time will shutdown the motor and exit the program.
+
+The generated .txt file should look similar to the example file in this repo:
+
+![output_file.png](resources/output_file.png)
 
 # External Resources
 - [logging Library](https://docs.python.org/3/library/logging.html)
 - [Pololu-Tic-Software Github](https://github.com/pololu/pololu-tic-software)
 - [Pololu Tic Manual](https://www.pololu.com/docs/0J71)
 - [Pololu Tic Resources](https://www.pololu.com/product/3131/resources)
-- [PyTic Github](https://github.com/AllenInstitute/pytic)
+- [ticlib Github](https://github.com/jphalip/ticlib)
 - [tic.h](https://github.com/pololu/pololu-tic-software/blob/master/include/tic.h)
 - [tic_protocol.h](https://github.com/pololu/pololu-tic-software/blob/a75c204a2255554e21cc5351c528d930ba5d2c38/include/tic_protocol.h)
 - [Tic Software and Drivers for Windows](https://www.pololu.com/file/0J1325/pololu-tic-1.6.2-win.msi)
--- autoTic.py
+++ autoTic.py
@@ -1,20 +1,247 @@
+import csv
+from datetime import datetime
 import libusb_package
+import os
+import signal
+import sys
+import readchar
 from ticlib import TicUSB
+import time
+
+# - Functions ------------------------------------------------
+
+# Energize motor
+
+
+def start():
+    tic.energize()
+    tic.exit_safe_start()
+    print('Energizing motor...')
+
+# De-energize motor and get error status
+
+
+def shutdown():
+    tic.enter_safe_start()
+    tic.deenergize()
+    print('Deenergizing motor...')
+    print(tic.variables.error_status)
+
+
+# Load in settings
+
+
+def load_settings():
+
+    variable_csv = r'run_parameters.csv'
+
+    with open(variable_csv, encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        global velocity_col
+        global holding_col
+
+        velocity_col = {'target_velocities': []}
+        holding_col = {'holding_time': []}
+
+        for record in reader:
+            velocity_col['target_velocities'].append(
+                int(record['target_velocities']))
+            holding_col['holding_time'].append(int(record['holding_time']))
+
+    print(f'Target Velocities: {velocity_col.values()}')
+    print(f'Holding Times (seconds): {holding_col.values()}.')
+
+# Enter settings manually
+
+
+def manual_settings():
+    get_target_velocities()
+    get_holding_times()
+
+# Get user input for target velocities
+
+
+def get_target_velocities():
+    global velocity_col
+
+    velocity_col = {'target_velocities': []}
+
+    while True:
+        try:
+            velocity_input = input('Enter target velocities (microsteps/ 10,000 seconds): \n '
+                                   'Please enter a list separated by a space \n')
+
+            velocity_list = velocity_input.split()
+
+            try:
+                for i in velocity_list:
+                    velocity_col['target_velocities'].append(int(i))
+            except ValueError:
+                print('Please use numbers.')
+
+        finally:
+            print(f'Target Velocities: {list(velocity_col.values())}')
+            break
+
+# Get user input for holding times
+
+
+def get_holding_times():
+    global holding_col
+
+    holding_col = {'holding_time': []}
+
+    while True:
+        try:
+            holding_input = input('Holding times for target velocities (secs): \n '
+                                  'Please enter a list separated by a space \n')
+
+            holding_list = holding_input.split()
+
+            try:
+                for i in holding_list:
+                    holding_col['holding_time'].append(int(i))
+            except ValueError:
+                print('Please use numbers.')
+
+        finally:
+            print(f'Holding Times (seconds): {list(holding_col.values())}.')
+            break
+
+# Retrieve current timestamp in %d-%m-%Y, %H:%M:%S format
+
+
+def current_time():
+    current_time = datetime.now()
+    time_stamp = current_time.timestamp()
+    date_time = datetime.fromtimestamp(time_stamp)
+    return date_time
+
+# When clicking Ctrl + C, confirm command and shutdown motor
+
+
+def handler(signum, frame):
+    print('\n')
+    msg = "Ctrl-c was pressed. Do you really want to exit? Y/N \n"
+    print(msg, end="", flush=True)
+    res = readchar.readchar()
+    if res == 'y' or res == 'Y':
+        print("Shutting down motor...")
+        shutdown()
+        exit(1)
+    else:
+        print("", end="\r", flush=True)
+        print(" " * len(msg), end="", flush=True)  # clear the printed line
+        print("    ", end="\r", flush=True)
+
+
+signal.signal(signal.SIGINT, handler)
+
+# - Initialization -------------------------------------------
 
 for dev in libusb_package.find(find_all=True):
     print(dev)
 
 tic = TicUSB()
 
+# - Configure Run Parameters ---------------------------------
+
+while True:
+    try:
+        core_name = input("Enter core name: ")
+    finally:
+        print(f'Core id: {core_name}')
+        break
+
+while True:
+    try:
+        endPrompt = input(
+            'Import run parameters (I) or enter manually (M)? Enter I or M. \n')
+    except ValueError:
+        print('Incorrect input')
+    finally:
+        if endPrompt == 'I' or endPrompt == 'i':
+            print("Importing from run_parameters.csv")
+            load_settings()
+            break
+        elif endPrompt == 'M' or endPrompt == 'm':
+            manual_settings()
+            break
+        else:
+            print('Incorrect input')
+
+
+# - Commence Run ---------------------------------------------
+
+# Zero current motor position
 tic.halt_and_set_position(0)
-tic.energize()
-tic.exit_safe_start()
 
-positions = [200, 150, 100, 50, 0]
-for position in positions:
-    tic.set_target_position(position)
-    while tic.get_current_position() != tic.get_target_position():
-        sleep(0.1)
+# Energize Motor
+start()
 
-tic.deenergize()
-tic.enter_safe_start()
+while True:
+    try:
+        endPrompt = input('Begin run? Y/N \n')
+    except ValueError:
+        print('Incorrect input')
+    finally:
+        if endPrompt == 'Y' or endPrompt == 'y':
+            print("Starting run...")
+            break
+        elif endPrompt == 'N' or endPrompt == 'n':
+            print('Take your time :)')
+        else:
+            print('Incorrect input')
+
+# Create new document to store time and velocity information
+with open(f'sediment experiment core {core_name} {current_time().date()}.txt', 'a', newline='') as output_file:
+
+    # Write header information to document
+    output_writer = csv.writer(output_file, delimiter='\t')
+    output_writer.writerow([f'Date: {current_time()}'])
+    output_writer.writerow([f'User: {os.getlogin()}'])
+    output_writer.writerow([f'Tic Device: '])
+    output_writer.writerow([f'Tic Serial Number:'])
+    output_writer.writerow(
+        [f'Target Velocities: {list(velocity_col.values())}'])
+    output_writer.writerow([f'Holding Times: {list(holding_col.values())}'])
+    output_writer.writerow('')
+    output_writer.writerow(['<<<Begin Data Collection>>>'])
+    output_writer.writerow(['time', 'current_velocity'])
+
+    counter = 0
+
+    # Iterate through list of target velocities
+    for key, value in velocity_col.items():
+        for i in value:
+            tic.set_target_velocity(i)
+            holding_time = int(holding_col['holding_time'][counter])
+            counter += 1
+            # Maintain current velocity for pre-determined holding time
+            while tic.get_current_velocity() != tic.get_target_velocity():
+                for t in range(holding_time, 0, -1):
+                    time.sleep(1)
+                    sys.stdout.write(
+                        f'Current velocity: {i}     Current time: {current_time()}    Time to velocity change: {t} seconds \n')
+                    # Clear printed line
+                    sys.stdout.flush()
+                    # Write current time and velocity to output file
+                    output_writer.writerow(
+                        [f'{current_time()}', f'{tic.get_current_velocity()}'])
+
+    # De-energize motor and get error status
+    while True:
+        try:
+            endPrompt = input('Stop Motor? Y/N  ')
+        except ValueError:
+            print('Incorrect input')
+        finally:
+            if endPrompt == 'Y' or endPrompt == 'y':
+                print('Shutting down motor...')
+                shutdown()
+                break
+            elif endPrompt == 'N' or endPrompt == 'n':
+                print('Use your spiral power!')
+            else:
+                print('Incorrect input')
